bad3321bed4db5da1a1f21bf85e2cb60ff2820f3
rehanjindran <RJ526@live.mdx.ac.uk>
2025-03-06 23:03:10 +0400

    Update broken_pirate.py

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,6 @@
-greeting = input("Hello, possible pirate! What's the password?)
-if greeting in ["Arrr!"):
-	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+greeting = input("Hello, possible pirate! What's the password? ")
+
+if greeting in ["Arrr!"]:
+    print("Go away, pirate.")
+else:
+    print("Greetings, hater of pirates!")
