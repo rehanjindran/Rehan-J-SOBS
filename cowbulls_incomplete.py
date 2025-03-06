2a7b2d65abe81d6518ccac8f2208f882a252d549
rehanjindran <RJ526@live.mdx.ac.uk>
2025-03-06 23:08:49 +0400

    Update cowbulls_incomplete.py

--- cowbulls_incomplete.py
+++ cowbulls_incomplete.py
@@ -1,32 +1,47 @@
 import random
 
 def compare_numbers(number, user_guess):
-    ## your code here
-    return cowbull
+    cows = 0
+    bulls = 0
+    
+    for i in range(4):
+        if user_guess[i] == number[i]:  
+            bulls += 1
+        elif user_guess[i] in number:  
+            cows += 1
+    
+    return cows, bulls  
 
-playing = True #gotta play the game
-number = str(random.randint(0,9999)) #random 4 digit number
+playing = True
+number = str(random.randint(1000, 9999))  
 guesses = 0
-print number
+print(number)  
 
-print("Let's play a game of Cowbull!") #explanation
+print("Let's play a game of Cowbull!")
 print("I will generate a number, and you have to guess the numbers one digit at a time.")
-print("For every number that exists in the sequence but is in wrong place, you get a cow. For every one in the right place, you get a bull.")
+print("For every number that exists in the sequence but is in the wrong place, you get a cow.")
+print("For every one in the right place, you get a bull.")
 print("The game ends when you get 4 bulls!")
-print("Type exit at any prompt to exit.")
+print("Type 'exit' at any prompt to exit.")
 
 while playing:
-    user_guess = raw_input("Give me your best guess!")
-    if user_guess == "exit":
+    user_guess = input("Give me your best 4-digit guess: ")
+    
+    if user_guess.lower() == "exit":
         break
-    cowbullcount = compare_numbers(number,user_guess)
-    guesses+=1
+    
+    if not user_guess.isdigit() or len(user_guess) != 4:  
+        print("Please enter a valid 4-digit number.")
+        continue
+    
+    cowbullcount = compare_numbers(number, user_guess)
+    guesses += 1
 
-    print("You have "+ str(cowbullcount[0]) + " cows, and " + str(cowbullcount[1]) + " bulls.")
+    print(f"You have {cowbullcount[0]} cows and {cowbullcount[1]} bulls.")
 
-    if cowbullcount[1]==4:
+    if cowbullcount[1] == 4:  
         playing = False
-        print("You win the game after " + str(guesses) + "! The number was "+str(number))
-        break #redundant exit
+        print(f"You win the game after {guesses} guesses! The number was {number}.")
+        break
     else:
         print("Your guess isn't quite right, try again.")
