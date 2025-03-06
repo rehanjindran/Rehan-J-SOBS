9a7f24a75cac606c3161a1de3e86fc6bb3374939
rehanjindran <RJ526@live.mdx.ac.uk>
2025-03-06 23:00:21 +0400

    Update broken_grades.py

--- broken_grades.py
+++ broken_grades.py
@@ -19,38 +19,34 @@
 # Grade: F
 # Student iis failing.
 
+
 exam_one = int(input("Input exam grade one: "))
+exam_two = int(input("Input exam grade two: "))
+exam_three = int(input("Input exam grade three: "))
 
-exam_two = input("Input exam grade two: "))
+grades = [exam_one, exam_two, exam_three]
 
-exam_3 = str(input("Input exam grade three: "))
-
-grades = [exam_one exam_two exam_three]
-sum = 0
-for grade in grade:
-  sum = sum + grade
-
-avg = sum / len(grdes)
+total = sum(grades)
+avg = total / len(grades)
 
 if avg >= 90:
     letter_grade = "A"
-elif avg >= 80 and avg < 90
+elif avg >= 80:
     letter_grade = "B"
-elif avg > 69 and avg < 80:
-    letter_grade = "C'
-elif avg <= 69 and avg >= 65:
+elif avg >= 70:
+    letter_grade = "C"
+elif avg >= 60:
     letter_grade = "D"
-elif:
+else:
     letter_grade = "F"
 
 for grade in grades:
-    print("Exam: " + str(grade))
+    print("Exam:", grade)
 
-    print("Average: " + str(avg))
+print("Average:", avg)
+print("Grade:", letter_grade)
 
-    print("Grade: " + letter_grade)
-
-if letter-grade is "F":
-    print "Student is failing."
+if letter_grade == "F":
+    print("Student is failing.")
 else:
-    print "Student is passing."
+    print("Student is passing.")
